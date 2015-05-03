4e27f0fd8c5bb98873f965dd5fba97ce2fe8cec8
mydearxym <mydearxym@qq.com>
2015-05-03 14:24:09 +0800

    add first route

--- phone_side/index.ios.js
+++ phone_side/index.ios.js
@@ -5,49 +5,39 @@
 'use strict';
 
 var React = require('react-native');
+var Main = require('./App/Components/Main');
+
 var {
   AppRegistry,
   StyleSheet,
+  NavigatorIOS,
   Text,
   View,
 } = React;
 
-var phone_side = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          欢迎来到我的世界!!-
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
-        </Text>
-      </View>
-    );
-  }
-});
 
 var styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#111111'
   },
 });
+
+//var phone_side = React.createClass({
+//var phone_side = React.createClass({
+class phone_side extends  React.Component{
+  render() {
+    return (
+      <NavigatorIOS 
+      style={styles.container}
+        initialRoute={{ 
+          title: '首页' ,
+          component:Main
+
+        }} />
+
+    );
+  }
+};
 
 AppRegistry.registerComponent('phone_side', () => phone_side);
