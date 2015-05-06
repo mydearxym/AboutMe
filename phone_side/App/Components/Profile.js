0f6bf00a02ae98c45c5864b7bd00dbcffa8e0781
mydearxym <mydearxym@qq.com>
2015-05-07 01:21:02 +0800

    add a separator

--- phone_side/App/Components/Profile.js
+++ phone_side/App/Components/Profile.js
@@ -1,5 +1,6 @@
 var React = require('react-native');
 var Badge = require("./Badge");
+var Separator = require("./helpers/Separator");
 
 var {
 	Text,
@@ -50,6 +51,9 @@ class Profile extends React.Component {
 				      <Text style={styles.rowTitle}> {this.getRowtitle(userInfo, item)} </Text>
 				      <Text style={styles.rowContent}> {userInfo[item]} </Text>
 				    </View>
+				    
+				    <Separator/>
+
 				   </View>
         )
 
